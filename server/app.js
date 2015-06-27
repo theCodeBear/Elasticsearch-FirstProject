68aa08ad95770ab202e333452b08d5893541b82c
Todd Kronenberg <toddkronenberg@gmail.com>
2015-06-26 17:07:56 -0700

    Set up an Elasticsearch client on server. Pinging the elasticsearch cluster by sending a HEAD request to "/?hello=elasticsearch". It works!

--- server/app.js
+++ server/app.js
@@ -6,6 +6,25 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
 
+// Elasticsearch stuff
+var elasticsearch = require('elasticsearch');
+var connectionString = 'localhost:9200';
+var client = new elasticsearch.Client({
+  host: connectionString,
+  log: 'trace'
+});
+// send a HEAD request to /?hello=elasticsearch
+client.ping({
+  requestTimeout: 30000,
+  //undocumented params are appended to the query string
+  hello: 'elasticsearch!'
+}, function(err) {
+  if (err)
+    console.error('elasticsearch cluster is down');
+  else
+    console.log('all is well with elasticsearch');
+});
+
 // running some basic Express middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
